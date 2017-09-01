fc54b2f566a7a6e4f029d6c9bf405ee8de33be6d
sahphae <sahphae+github@gmail.com>
2017-09-01 12:08:13 +0200

    changed inndentation of function body

--- legendreFunctions.m
+++ legendreFunctions.m
@@ -1,15 +1,15 @@
-function P = legendreFunctions(theta, max_deg)
 % m-function wrapper for parsing the inputs
-if (nargin < 2)
-	error('not enough input arguments');
-elseif (nargin > 2)
-	error('too many input arguments');
-end
+function P = legendreFunctions(theta, max_deg)
+	if (nargin < 2)
+		error('not enough input arguments');
+	elseif (nargin > 2)
+		error('too many input arguments');
+	end
 
-if (~isreal(theta) || ~isscalar(theta))
-	error('theta muste be a real scalar');
-elseif (~isreal(max_deg) || ~isscalar(max_deg))
-	error('max_deg must be a real scalar');
-end
+	if (~isreal(theta) || ~isscalar(theta))
+		error('theta muste be a real scalar');
+	elseif (~isreal(max_deg) || ~isscalar(max_deg))
+		error('max_deg must be a real scalar');
+	end
 
-P = legendreFunctionsmx(theta, max_deg);
+	P = legendreFunctionsmx(theta, max_deg);
